16ff96df670f0da7b268d6a22f42d44036c7d40d
Colores <nicolores.ga@gmail.com>
2021-12-30 02:05:40 -0300

    NAVBAR

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// smooth scroll
+
+$(window).on("scroll", function(){
+    var changeColor = $(window).scrollTop();
+    if(changeColor > 0){
+$(".navbar").css("background", "rgba(0,0,0)");
+}
+else{
+    $(".navbar").css("background","rgba(250,250,250,0)");
+}
+});
