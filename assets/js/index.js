dad05e72bfcebfd8a81f52172f55ef9a3a842234
Colores <nicolores.ga@gmail.com>
2021-12-30 19:14:40 -0300

    Destacados

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,5 +10,9 @@ $(".navbar").css("background","rgba(0,0,0)");
         }
       });
 
-
-      
+// TOOLTIP
+var tooltipTriggerList =
+[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
